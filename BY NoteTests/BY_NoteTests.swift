7677b160ba9b6852fbfddce0fd80ae9edbedee5c
Johnny bian <johnnybian@wifi-etudiants2831.utt.fr>
2017-11-22 21:10:51 +0100

    Initial Commit

---
+++ BY NoteTests/BY_NoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  BY_NoteTests.swift
+//  BY NoteTests
+//
+//  Created by Johnny bian on 2017/11/22.
+//  Copyright © 2017年 Johnny bian Peirun Yu. All rights reserved.
+//
+
+import XCTest
+@testable import BY_Note
+
+class BY_NoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
